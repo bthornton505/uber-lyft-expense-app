396e6f23b5e7bfd5d5445438fd8f070fb7a5e245
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-13 14:02:26 -0700

    added month and year column

--- db/migrate/20181212212008_create_expense_reports.rb
+++ db/migrate/20181212212008_create_expense_reports.rb
@@ -1,9 +1,11 @@
 class CreateExpenseReports < ActiveRecord::Migration[5.2]
   def change
     create_table :expense_reports do |t|
-      t.string :name 
+      t.string :month
+      t.integer :year 
       t.integer :user_id
 
+      t.timestamps
     end
   end
 end
