1ff6bbf7a6c042b8a26961ac0732772a02122354
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-13 14:02:09 -0700

    took away category_id column

--- db/migrate/20181212211703_create_expenses.rb
+++ db/migrate/20181212211703_create_expenses.rb
@@ -3,7 +3,6 @@ class CreateExpenses < ActiveRecord::Migration[5.2]
     create_table :expenses do |t|
       t.string :month
       t.integer :cost
-      t.integer :category_id
       t.integer :expense_report_id
 
       t.timestamps
