c11cb82357530b549d4d6db99c3e13ec59274387
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-14 13:08:54 -0700

    added class method that uses auth hash to find or create user from github

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ActiveRecord::Base
   has_many :expense_reports
   has_many :expenses, through: :expense_reports
 
+  def self.find_or_create_by_omniauth(auth_hash)
+        self.where(:name => auth_hash["info"]["name"]).first_or_create do |user|
+            user.password = SecureRandom.hex
+        end
+    end
+
 end
