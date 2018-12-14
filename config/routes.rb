679e370b9dfe818ff7f419a7a5ec318535304970
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-14 14:34:21 -0700

    trying to figure out nested routes for expense_reports inside users

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,13 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  resources :users
+  resources :users do
+    resources :expense_reports
+  end
+
+  # resources :expense_reports do
+  #   resources :expenses
+  # end
 
   # Routes for omniauth-github login
   get '/auth/github/callback' => 'sessions#create'
