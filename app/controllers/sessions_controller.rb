81f207136a96f54a42347a306006d699122b1507
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-14 13:08:20 -0700

    added new conditional in create action to allow users to login with github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,14 +5,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(name: params[:session][:name])
-    # binding.pry
-    if @user && @user.authenticate(params[:session][:password])
+
+    if auth_hash = request.env["omniauth.auth"]
+      @user = User.find_or_create_by_omniauth(auth_hash)
       session[:user_id] = @user.id
       redirect_to user_path(@user)
     else
-      render login_path
+
+      @user = User.find_by(name: params[:session][:name])
+      if @user && @user.authenticate(params[:session][:password])
+        session[:user_id] = @user.id
+        redirect_to user_path(@user)
+      else
+        render login_path
+      end
     end
+
   end
 
   def destroy
