5e6e77d9c483d4bc10306f20867aec04c88f9711
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-14 15:11:09 -0700

    added :shallow option to expense_reports resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,18 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
 
-  get '/login' => 'sessions#new'
-  post '/login' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
-
   resources :users do
-    resources :expense_reports
+    resources :expense_reports, shallow: true 
   end
 
   # resources :expense_reports do
   #   resources :expenses
   # end
 
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   # Routes for omniauth-github login
   get '/auth/github/callback' => 'sessions#create'
 
