c7ae1ce2168f425004dfdc89f08d8cf30065a347
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-14 21:21:02 -0700

    fixed new and index action to store @user and @xpense_report variables properly for creating expense_reports

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -1,16 +1,32 @@
 class ExpenseReportsController < ApplicationController
 
-  def index
+  def show
+    @expense_report = ExpenseReport.find_by(id: params[:id])
+  end
 
+  def index
+    if params[:user_id] && current_user.id == params[:user_id].to_i
+      @user = current_user
+      @expense_reports = @user.expense_reports
+    elsif params[:user_id]
+      redirect_to expense_report_path
+    else
+      @expense_reports = ExpenseReport.all
+    end
   end
 
   def new
-    @expense_report = ExpenseReport.new
+    if params[:user_id] && current_user.id == params[:user_id].to_i
+      @user = current_user
+      @expense_report = ExpenseReport.new(user_id: params[:user_id])
+    else
+      redirect_to expense_reports_path
+    end
   end
 
   def create
     @expense_report = ExpenseReport.create(expense_report_params)
-    binding.pry
+    # binding.pry
     if @expense_report.valid?
       flash[:message] = "Successfully created new expense report!"
       redirect_to expense_report_path(@expense_report)
