55fcf58e0d122bee985ca0ece54bcb2b062e5d73
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-18 12:36:27 -0700

    added create action for expenses

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,4 +5,24 @@ class ExpensesController < ApplicationController
     @expense = Expense.new(:expense_report => @expense_report)
   end
 
+  def create
+    @expense = Expense.new(expense_params)
+    @expense_report = ExpenseReport.find(params[:expense_report_id])
+
+    if @expense.save
+      flash[:message] = "Successfully created new expense"
+      redirect_to expense_report_path(@expense_report)
+    else
+      render :new
+    end
+    
+  end
+
+
+  private
+
+  def expense_params
+    params.require(:expense).permit(:month, :cost, :expense_report_id)
+  end
+
 end
