50a6a42b623c387513ad92105c8a35a85c2735ce
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-18 18:10:29 -0700

    added create action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,18 @@
 class CategoriesController < ApplicationController
 
   def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.valid?
+      @category.save
+    else
+      flash[:message] = "Not a valid category"
+      redirect_to new_expense_report_expense_path(@user.expense_reports)
+    end 
 
   end
 
