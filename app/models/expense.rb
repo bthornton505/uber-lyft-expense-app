dd1c1ebf34182d5168b530fe08635de322898886
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-18 19:20:58 -0700

    updated new relationship

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,6 +1,6 @@
 class Expense < ActiveRecord::Base
   belongs_to :expense_report
-  has_many :expense_categories
-  has_many :categories, through: :expense_categories
+  has_many :comments
+  has_many :categories, through: :comments
 
 end
