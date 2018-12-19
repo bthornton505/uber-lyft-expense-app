4751d811eab7ecfcd301d10987b92014e394900b
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-18 19:20:38 -0700

    added some comments for possible ideas

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,18 @@
 class Category < ActiveRecord::Base
-  has_many :expense_categories
-  has_many :expenses, through: :expense_categories
+  has_many :comments
+  has_many :expenses, through: :comments
+
+# I want to hard code the possible categories but still have them persist to the category table
+
+  # CATEGORIES = [
+  #   "Gas",
+  #   "Miles",
+  #   "Car Insurance",
+  #   "Car Payment",
+  #   "Car Wash",
+  #   "Maintenance",
+  #   "Phone Payment",
+  #   "Miscellaneous"
+  # ]
+
 end
