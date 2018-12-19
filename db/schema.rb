aff5ddf1f23c38adf83e1e2640590bc674151d6f
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-18 19:24:21 -0700

    new updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_220957) do
+ActiveRecord::Schema.define(version: 2018_12_19_021817) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 2018_12_12_220957) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "expense_categories", force: :cascade do |t|
-    t.string "note"
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
     t.integer "expense_id"
     t.integer "category_id"
     t.datetime "created_at", null: false
