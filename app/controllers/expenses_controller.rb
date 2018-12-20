8b0162c8ad1911dc25bc6425b7932d318709d024
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-20 08:15:44 -0700

    working on nested form

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -3,20 +3,25 @@ class ExpensesController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     @expense = Expense.find_by(id: params[:id])
+    # @expense.category = Expense.comments
   end
 
   def new
     @expense_report = ExpenseReport.find(params[:expense_report_id])
     @expense = Expense.new(:expense_report => @expense_report)
-    @category = Category.new
-    @comment = Comment.new
+    @category = Category.find_by(params[:id])
+    @categories = Category.all
+    @expense.comments.build( expense_id: @expense.id, category_id: @category.id )
+
+    # @category = Category.new
+    # @comment = Comment.new
   end
 
   def create
     @expense = Expense.new(expense_params)
-    @expense_report = ExpenseReport.find(params[:expense_report_id])
-    @category = Category.find_by(params[:id])
-    @comment = Comment.new
+    # @expense_report = ExpenseReport.find(params[:expense_report_id])
+    # @category = Category.find_by(params[:id])
+    # @comment = Comment.new
 
     if @expense.save
       flash[:message] = "Successfully created new expense"
@@ -31,7 +36,7 @@ class ExpensesController < ApplicationController
   private
 
   def expense_params
-    params.require(:expense).permit(:month, :cost, :expense_report_id)
+    params.require(:expense).permit(:month, :cost, :expense_report_id, :comment_attributes => [:description, :expense_id, :category_id])
   end
 
 end
