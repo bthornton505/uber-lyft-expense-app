3b542c2d3804cab71a0f1fca0ff54442e5a33256
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-20 08:16:07 -0700

    added comments_attributes and category methods

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,4 +3,18 @@ class Expense < ActiveRecord::Base
   has_many :comments
   has_many :categories, through: :comments
 
+  # accepts_nested_attributes_for :comments
+
+  def comments_attributes=(comments_attributes)
+    raise comments_attributes.inspect
+    comments_attributes.each do |i, comment_attributes|
+      self.comments.build(comment_attributes)
+    end
+  end
+
+
+  def category=(category_id)
+    self.category = Category.find_by(id: category_id)
+  end
+
 end
