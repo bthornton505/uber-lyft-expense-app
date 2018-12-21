cd07d553a6f5f027cf8aeda6d6937f8a030d897e
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-20 22:44:51 -0700

    changed render to redirect_to login_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
         session[:user_id] = @user.id
         redirect_to user_path(@user)
       else
-        render login_path
+        redirect_to login_path
       end
     end
 
