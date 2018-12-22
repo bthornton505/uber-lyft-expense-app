e01a6e4537ca15c4f3f2ad04ed30d8577d7b086c
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-22 09:53:58 -0700

    cleaned up file and created helper_methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
 
   def logged_in?
     !!current_user
@@ -9,16 +10,8 @@ class ApplicationController < ActionController::Base
     redirect_to root_path unless logged_in?
   end
 
-  private
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-  helper_method :current_user
-
-  # def current_expense_report
-  #   @current_expense_report ||= ExpenseReport.find(@expense_report.id)
-  # end
-  # helper_method :current_expense_report
 
 end
