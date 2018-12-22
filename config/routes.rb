878dc472beee3cf3a2bed6110d60ce4185abe888
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-22 10:31:18 -0700

    created new nested routes for expenses

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,12 @@ Rails.application.routes.draw do
   get '/expense_reports/:expense_report_id/expenses/new' => 'expenses#new', :as => :new_expense_report_expense
   get '/expense_reports/:expense_report_id/expenses' => 'expenses#index', :as => :expense_report_expenses
   post '/expense_reports/:expense_report_id/expenses' => 'expenses#create'
+  get '/expense_reports/:expense_report_id/expenses/:expense_id/edit' => 'expenses#edit', :as => :edit_expense
+  get '/expense_reports/:expense_report_id/expenses/:expense_id' => 'expenses#show', :as => :expense
+  patch '/expense_reports/:expense_report_id/expenses/:expense_id' => 'expenses#update'
+  # put '/'
 
-  resources :expenses, only: [:show, :edit, :update, :destroy]
+  # resources :expenses, only: [:show, :edit, :update, :destroy]
 
   resources :categories
 
