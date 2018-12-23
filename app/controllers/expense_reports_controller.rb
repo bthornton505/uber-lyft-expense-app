a7544ee15f00d229430b405ab34ac92824c8ad04
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-23 11:26:09 -0700

    added new before_action and cleaned up controller

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -1,4 +1,5 @@
 class ExpenseReportsController < ApplicationController
+  before_action :logged_in_required
 
   def show
     @expense_report = ExpenseReport.find_by(id: params[:id])
@@ -6,29 +7,30 @@ class ExpenseReportsController < ApplicationController
   end
 
   def index # Logic still needs work. Eventually needs to be refactored.
-    if params[:user_id] && current_user.id == params[:user_id].to_i
+    # if params[:user_id] && current_user.id == params[:user_id].to_i
       @user = current_user
       @expense_reports = @user.expense_reports
-    elsif params[:user_id] != current_user # I want this to redirect_to a users profile if they try to perform action on another users page
-      redirect_to root_url                # Also show them a warning that says this action cannot be done
-    else
-      redirect_to user_path(@user)
-    end
+    # elsif params[:user_id] != current_user # I want this to redirect_to a users profile if they try to perform action on another users page
+    #   redirect_to root_url                # Also show them a warning that says this action cannot be done
+    # else
+    #   redirect_to user_path(@user)
+    # end
   end
 
   def new # Logic still needs work. Eventually needs to be refactored.
-    if params[:user_id] && current_user.id == params[:user_id].to_i
+    # if params[:user_id] && current_user.id == params[:user_id].to_i
       @user = current_user
       @expense_report = ExpenseReport.new(user_id: params[:user_id])
-    else
-      redirect_to root_url
-    end
+    # else
+    #   redirect_to root_url
+    # end
   end
 
   def create
-    @expense_report = ExpenseReport.create(expense_report_params)
-    # binding.pry
-    if @expense_report.valid?
+    @expense_report = ExpenseReport.new(expense_report_params)
+    binding.pry
+    if @expense_report.save
+      binding.pry
       flash[:message] = "Successfully created new expense report!"
       redirect_to expense_report_path(@expense_report)
     else
