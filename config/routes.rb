0264b2dc473e1eafa74dde211ef945f894f13adb
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-24 13:47:21 -0700

    properly nested my routes for expenses and cleaned up some commented code

--- config/routes.rb
+++ config/routes.rb
@@ -3,33 +3,16 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   resources :users do
-    resources :expense_reports, shallow: true
+    resources :expense_reports, only: [:new, :create, :index]
   end
 
-# Routes to create expenses under associated expense_report
-  get '/expense_reports/:expense_report_id/expenses' => 'expenses#index', :as => :expense_report_expenses
-  post '/expense_reports/:expense_report_id/expenses' => 'expenses#create'
-  get '/expense_reports/:expense_report_id/expenses/new' => 'expenses#new', :as => :new_expense_report_expense
-  # get '/expense_reports/:expense_report_id/expenses/:expense_id/edit' => 'expenses#edit', :as => :edit_expense_report_expense
-  # get '/expense_reports/:expense_report_id/expenses/:expense_id' => 'expenses#show', :as => :expense_report_expense
-  # patch '/expense_reports/:expense_report_id/expenses/:expense_id' => 'expenses#update'
-
-  # get '/expense_reports/:expense_report_id/expenses/new' => 'expenses#new', :as => :new_expense_report_expense
-  # get '/expense_reports/:expense_report_id/expenses' => 'expenses#index', :as => :expense_report_expenses
-  # post '/expense_reports/:expense_report_id/expenses' => 'expenses#create'
-
-  resources :expenses, only: [:show, :edit, :update, :destroy]
-
-
-
-  # resources :expenses, only: [:show, :edit, :update, :destroy]
+  resources :expense_reports, only: [:show, :edit, :update, :destroy] do
+    resources :expenses
+  end
 
   resources :categories
 
-# Routes to create a new comment for an expense
   resources :comments
-  # get '/expenses/:expense_id/comments/new' => 'comments#new', :as => :new_expense_comment
-  # post '/expenses/:expense_id/comments' => 'comments#create', :as => :expense_comment
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
