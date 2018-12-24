367dfcd178bebc65d3a75ac7981e01645ebecf68
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-24 13:45:23 -0700

    finally got edit/update actions to work. Updated params to find the associated expense report

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -2,45 +2,48 @@ class ExpensesController < ApplicationController
   before_action :logged_in_required
 
   def show
-    @expense_report = ExpenseReport.find_by(params[:id])
+    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
     @expense = Expense.find_by(id: params[:id])
   end
 
   def new
-    @expense_report = ExpenseReport.find_by(params[:id])
+    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
     @expense = Expense.new(:expense_report => @expense_report)
-
-    # binding.pry
-
-    # @expense_report = ExpenseReport.find_by(params[:expense_report_id])
-    # @expense = Expense.new(:expense_report => @expense_report)
-    # binding.pry
     @category = Category.find_by(params[:id])
     @categories = Category.all
     @expense.comments.build( expense_id: @expense.id, category_id: @category )
   end
 
   def create
+    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
     @expense = Expense.new(expense_params)
-    # binding.pry
+
     if @expense.save
       flash[:message] = "Successfully created new expense"
-      redirect_to expense_path(@expense)
+      redirect_to expense_report_expense_path(@expense_report.id, @expense)
     else
       render :new
     end
   end
 
   def edit
-    @expense_report = ExpenseReport.find_by(params[:id])
-    @expense = Expense.find_by(params[:id])
+    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
+    @expense = Expense.find_by(id: params[:id])
     @category = Category.find_by(params[:id])
     @categories = Category.all
 
   end
 
   def update
+    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
+    @expense = Expense.find_by(id: params[:id])
 
+    if @expense.update(expense_params)
+      redirect_to expense_report_expense_path(@expense_report.id, @expense)
+    else
+      render :edit
+    end
+    
   end
 
   private
