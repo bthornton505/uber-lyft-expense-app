c0e812c8a420347728e56b87679b3dc1fbf35d7b
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-26 20:00:07 -0700

    fixed some spacing for my find_or_create_by_omniauth method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ActiveRecord::Base
   has_many :expenses, through: :expense_reports
 
   def self.find_or_create_by_omniauth(auth_hash)
-        self.where(:name => auth_hash["info"]["name"]).first_or_create do |user|
-            user.password = SecureRandom.hex
-        end
+    self.where(:name => auth_hash["info"]["name"]).first_or_create do |user|
+      user.password = SecureRandom.hex
     end
+  end
 
 end
