d623f3dca6ec2b1be1ef5f939bfd9e67c622e641
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-26 18:39:28 -0700

    cleaned up commented code that wasn't being used anymore

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -6,31 +6,20 @@ class ExpenseReportsController < ApplicationController
     @expenses = Expense.all.select {|e| e.expense_report_id == @expense_report.id}
   end
 
-  def index # Logic still needs work. Eventually needs to be refactored.
-    # if params[:user_id] && current_user.id == params[:user_id].to_i
-      @user = current_user
-      @expense_reports = @user.expense_reports
-    # elsif params[:user_id] != current_user # I want this to redirect_to a users profile if they try to perform action on another users page
-    #   redirect_to root_url                # Also show them a warning that says this action cannot be done
-    # else
-    #   redirect_to user_path(@user)
-    # end
+  def index
+    @user = current_user
+    @expense_reports = @user.expense_reports
   end
 
-  def new # Logic still needs work. Eventually needs to be refactored.
-    # if params[:user_id] && current_user.id == params[:user_id].to_i
-      @user = current_user
-      @expense_report = ExpenseReport.new(user_id: params[:user_id])
-    # else
-    #   redirect_to root_url
-    # end
+  def new
+    @user = current_user
+    @expense_report = ExpenseReport.new(user_id: params[:user_id])
   end
 
   def create
     @expense_report = ExpenseReport.new(expense_report_params)
-    # binding.pry
+    
     if @expense_report.save
-      # binding.pry
       flash[:message] = "Successfully created new expense report!"
       redirect_to expense_report_path(@expense_report)
     else
