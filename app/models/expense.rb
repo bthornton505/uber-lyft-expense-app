1c64a58b925a70f770d79239be7d1b4e77ca320e
Brenden Thornton <thorntonbrenden@gmail.com>
2018-12-27 18:13:24 -0700

    added presence validation for month and cost

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -3,6 +3,9 @@ class Expense < ActiveRecord::Base
   has_many :comments
   has_many :categories, through: :comments
 
+  validates :month, presence: true
+  validates :cost, presence: true 
+
 
   def comments_attributes=(comments_attributes)
     comments_attributes.each do |i, comment_attributes|
