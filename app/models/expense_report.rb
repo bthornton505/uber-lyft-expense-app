806fa4dd85a5acac01bde636f7dad339a031d2c3
Brenden Thornton <thorntonbrenden@gmail.com>
2019-01-01 19:26:17 -0700

    trying to tweek scope methods to make them work the way i want

--- app/models/expense_report.rb
+++ app/models/expense_report.rb
@@ -2,16 +2,18 @@ class ExpenseReport < ActiveRecord::Base
   has_many :expenses
   belongs_to :user
 
-  validates :month, presence: true 
-  validates :year, presence: true
-  validates :user_id, presence: true
+  validates :month, :year, :user_id, presence: true
+  # validates :year, presence: true
+  # validates :user_id, presence: true
 
   # Bottom line was original idea but found better solution to orginizing expense reports
   # scope :by_chronological_order, -> { order(year: :desc)}
   scope :recently_added, -> { order(created_at: :desc) }
+  # scope :current_report, -> { (created_at: :desc) }
+  scope :by_year, -> { order(year: :desc)}
 
-  def abc # Still working on a method to make sure the months are kept in chronoligal order. There current scope method does a pretty good job though
-    self.month.sort_by { |e| e.month.downcase }
+  def current_expense_report
+    self.recently_added.first
   end
 
 end
