1d01ce5fc371cabf65e127b14141e98dc2f4fe38
Brenden Thornton <thorntonbrenden@gmail.com>
2019-01-02 17:07:51 -0700

    cleaned up some code

--- app/models/expense_report.rb
+++ app/models/expense_report.rb
@@ -2,9 +2,9 @@ class ExpenseReport < ActiveRecord::Base
   has_many :expenses
   belongs_to :user
 
-  validates :month, :year, :user_id, presence: true
-  # validates :year, presence: true
-  # validates :user_id, presence: true
+  validates :month, presence: true
+  validates :year, presence: true
+  validates :user_id, presence: true
 
   scope :recently_added, -> { order(created_at: :desc) }
   scope :by_year, -> { order(year: :desc)}
