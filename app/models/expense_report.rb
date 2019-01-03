6c06b96afd279223154137ded216f546bf862a17
Brenden Thornton <thorntonbrenden@gmail.com>
2019-01-03 14:06:23 -0700

    updated the by_year scope method

--- app/models/expense_report.rb
+++ app/models/expense_report.rb
@@ -7,10 +7,15 @@ class ExpenseReport < ActiveRecord::Base
   validates :user_id, presence: true
 
   scope :recently_added, -> { order(created_at: :desc) }
-  scope :by_year, -> { order(year: :desc)}
+  scope :by_year, -> { select(:id, :month, :year).where("year = ?", "2019") }
+  # ExpenseReport.select(:id, :year).where(year: 2019)
 
   def self.current_expense_report
     self.recently_added.first
   end
+
+  # def self.by_year
+  #   self.find_by(year: self.year)
+  # end
 
 end
