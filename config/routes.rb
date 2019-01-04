fb3e3ee3376499e0fb7a28792e7eb1f6a834dd7c
Brenden Thornton <thorntonbrenden@gmail.com>
2019-01-04 11:30:12 -0700

    commented out some code I don't need

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   get '/expense_reports/by_year' => 'expense_reports#by_year'
-  get '/users/:user_id/expense_reports/current_report' => 'expense_reports#current_report', as: "users_current_expense_report"
+  # get '/users/:user_id/expense_reports/current_report' => 'expense_reports#current_report', as: "users_current_expense_report"
 
   resources :users do
     resources :expense_reports, only: [:new, :create, :index]
@@ -14,12 +14,12 @@ Rails.application.routes.draw do
   end
 
   resources :categories
-
   resources :comments
 
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+
   # Routes for omniauth-github login
   get '/auth/github/callback' => 'sessions#create'
 
