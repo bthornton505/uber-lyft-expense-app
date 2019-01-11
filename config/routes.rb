12e80c887a449a794d8b3b42381696a5a96f504b
Brenden Thornton <thorntonbrenden@gmail.com>
2019-01-11 10:19:13 -0700

    created routes for comments new/create

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
     resources :expenses
   end
 
+  resources :comments, only: [:new, :create]
+
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
