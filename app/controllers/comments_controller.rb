d7baa74c185c40b62ada5af58b1545e314262696
Brenden Thornton <thorntonbrenden@gmail.com>
2019-01-11 10:18:52 -0700

    added comments controller with new and create action

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,31 @@
+class CommentsController < ApplicationController
+  before_action :login_required
+
+  def new
+    @expense = Expense.find_by(id: params[:expense_id])
+    @comment = @expense.comments.build
+    @categories = Category.all
+  end
+
+  def create
+    binding.pry
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      flash[:success] = "Successfully added comment."
+      redirect_to expense_report_expense_path(@comment.expense.expense_report, @comment.expense)
+    else
+      flash[:alert] = "Something went wrong creating this comment. Try again."
+      @categories = Category.all
+      render :new
+    end
+
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:description, :expense_id, :category_id)
+  end
+
+end
