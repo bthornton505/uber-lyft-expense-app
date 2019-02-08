ef1ebc79be4823670bdeb5dcc3fb1162c164dcbe
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-08 15:44:48 -0700

    working on function to replace text in table with queried json objects

--- app/assets/javascripts/expense_report.js
+++ app/assets/javascripts/expense_report.js
@@ -3,18 +3,29 @@ $(function() {
 })
 
 // Variables
-let rootURL = 'https://localhost:3000/expense_reports/by_year?utf8=%E2%9C%93&year='
+let searchURL = 'https://localhost:3000/expense_reports/by_year?utf8=%E2%9C%93&year='
 
 // This will be used to search reports by year
 
 function searchByYear() {
-  $('#year').on('change', function(e) {
-    e.preventDefault();
-
+  $('#year').on('change', function() {
     let year = this.value;
+    // I now have the search parameter in a variable and use it to query the database
+    $.get(searchURL + year, function(data) {
+      let reportsTable = $('#expense-report-table');
+      reportsTable.empty()
 
-    $.get(rootURL + year, function(data) {
-      console.log(data);
+      let searchResults = data.map(function(report) {
+        let reportMonth = $('td#expense-report-month').text(report.month);
+        let reportYear = $('td#expense-report-year').text(report.year);
+        let reportLink = $('td#expense-report-link').text(`<a href="/expense_reports/${report.id}">View</a>`);
+
+        return reportMonth;
+        return reportYear;
+        return reportLink;
+      });
+      // After sending a get request for the reports, I need to hijack the submit button
+      // which will then arrange the data into the table properly (i hope)
     })
   })
 }
