56179ee5ffa565f7a85fbf0cccc8abfc2bf29ead
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-11 15:21:24 -0700

    got my ajax request for indexed reports to work

--- app/assets/javascripts/expense_report.js
+++ app/assets/javascripts/expense_report.js
@@ -4,7 +4,7 @@ $(function() {
 
 // Variables
 let searchURL = 'https://localhost:3000/expense_reports/by_year?utf8=%E2%9C%93&year='
-
+let rootURL = 'https://localhost:3000'
 // This will be used to search reports by year
 
 function searchByYear() {
@@ -12,47 +12,24 @@ function searchByYear() {
     let year = this.value;
     // I now have the search parameter in a variable and use it to query the database
     $.get(searchURL + year, function(data) {
-      console.log(data)
+      // Clear the table
+      let reportTable = $('#expense-report-table')
+      reportTable.empty();
 
-      clearReportTable();
-
-      let searchResults = data.forEach(function(report) {
-        debugger
+      let searchResults = data.map(function(report) {
+        // debugger
         console.log(report)
-        // let reportMonth = $('td#expense-report-month').text(report.month);
-        // let reportYear = $('td#expense-report-year').text(report.year);
-        // let reportLink = $('td#expense-report-link').text(`<a href="/expense_reports/${report.id}">View</a>`);
+        let result = "";
+        result += '<tr id="' + report.id + '">';
+        result += '<td id="expense-report-month">' + report.month + '</td>';
+        result += '<td id="expense-report-year">' + report.year + '</td>';
+        result += `<td id="expense-report-year"><a href="/expense_reports/${report.id}">View</a></td>`;
 
-
-        // return reportMonth;
-        // return reportYear;
-        // return reportLink;
+        return result;
       });
 
+      reportTable.append(searchResults);
 
-      // let searchResults = data.map(function(report) {
-      //   let result = $('td#expense-report-month').text(report.month);
-      //   result += $('td#expense-report-year').text(report.year);
-      //   result += $('td#expense-report-link').text(`<a href="/expense_reports/${report.id}">View</a>`);
-      //
-      //
-      //   return result;
-      // });
-
-      // console.log(searchResults)
-      // After sending a get request for the reports, I need to hijack the submit button
-      // which will then arrange the data into the table properly (i hope)
     })
   })
-}
-
-function clearReportTable() {
-  let reportsTableMonth = $('td#expense-report-month')
-  let reportsTableYear = $('td#expense-report-year')
-  let reportsTableLink = $('td#expense-report-link')
-  // The varialble above needs to find the specific tds that need to be emptied
-  // so that I can fill them in with json data
-  reportsTableMonth.empty()
-  reportsTableYear.empty()
-  reportsTableLink.empty()
 }
