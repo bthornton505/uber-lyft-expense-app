bc6c325392b9d32de7fd61818c949b88f9421ab7
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-13 16:37:21 -0700

    implemented some relationships to help build proper api endpoint, still not working properly though

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,4 +1,5 @@
 class ExpenseSerializer < ActiveModel::Serializer
   attributes :id, :month, :cost, :expense_report_id
   belongs_to :expense_report
+  has_many :categories, serializer: ExpenseCategorySerializer
 end
