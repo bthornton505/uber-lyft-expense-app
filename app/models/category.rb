d6d946e1fa54334c0b29ee52359d79ca2ad07299
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-15 13:12:50 -0700

    updated relationship to has_many_belongs_to expenses

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveRecord::Base
   # Categories will no longer have comments associated with it, only expenses
-  has_many :comments
+  # has_many :comments
   # This will change to a belongs_to relationship
-  has_many :expenses, through: :comments
+  has_and_belongs_to_many :expenses
 
 end
