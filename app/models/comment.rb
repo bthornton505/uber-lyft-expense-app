a4b77121a2fd433bbb824f2f5f19c75609ccdf16
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-15 13:12:31 -0700

    removed relationship to categories

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :expense
-  # Comment will no longer belong to a category  
-  belongs_to :category
+  # Comment will no longer belong to a category
+  # belongs_to :category
 
   validates :description, presence: true
 end
