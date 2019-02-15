070fd0f1ae1a18a9f6aca3673d371fceb76a7ee2
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-15 14:37:13 -0700

    updated db to express relationships properly

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_15_185304) do
+ActiveRecord::Schema.define(version: 2019_02_15_181402) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "categories_expenses", id: false, force: :cascade do |t|
-    t.integer "expense_id"
-    t.integer "category_id"
-    t.index ["category_id"], name: "index_categories_expenses_on_category_id"
-    t.index ["expense_id"], name: "index_categories_expenses_on_expense_id"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -43,6 +36,7 @@ ActiveRecord::Schema.define(version: 2019_02_15_185304) do
   create_table "expenses", force: :cascade do |t|
     t.string "month"
     t.integer "cost"
+    t.integer "category_id"
     t.integer "expense_report_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
