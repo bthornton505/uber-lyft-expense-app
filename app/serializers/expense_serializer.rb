5698b87a69b22992dcaa2e8cafb08b1d9e91c1d9
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-18 14:31:12 -0700

    added comment attribute to add data to json object

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,5 +1,5 @@
 class ExpenseSerializer < ActiveModel::Serializer
-  attributes :id, :month, :cost, :expense_report_id, :category
+  attributes :id, :month, :cost, :expense_report_id, :category, :comments
   belongs_to :expense_report
-  has_many :categories
+  
 end
