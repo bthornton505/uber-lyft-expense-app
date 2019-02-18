dc01761a7d6d49c9818ca4b4443396d533056b2c
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-17 17:11:01 -0700

    rendered multiple objects to have access to json data for js script

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -26,10 +26,10 @@ class ExpenseReportsController < ApplicationController
   def show
     @expense_report = ExpenseReport.find_by(id: params[:id])
     @expenses = Expense.all.select {|e| e.expense_report_id == @expense_report.id}
-    @categories = Expense.includes(:category).map {|e| e.category.expenses if e.expense_report_id == @expense_report.id }
+    @categories = Expense.includes(:category).map {|e| e.category if e.expense_report_id == @expense_report.id }
     respond_to do |format|
       format.html { render :show }
-      format.json { render json: @expense_report, status: 201 }
+      format.json { render json: { expense_report: @expense_report, expenses: @expenses, categories: @categories }, status: 201 }
     end
     # binding.pry
   end
