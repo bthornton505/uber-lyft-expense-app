7168f1036ed75633e1c09d3f0c1b2a555e60d90e
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-19 14:02:51 -0700

    cleaned up controller with before actions to set expense, categories, and report

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,9 +1,9 @@
 class ExpensesController < ApplicationController
-  before_action :login_required
+  before_action :login_required, :set_report
+  before_action :set_expense, only: [:show, :edit, :update, :delete]
+  before_action :set_categories, only: [:new, :create, :edit, :update]
 
   def show
-    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
-    @expense = Expense.find_by(id: params[:id])
     @comment = @expense.comments.build
     @comments = @expense.comments.all
     respond_to do |format|
@@ -13,56 +13,36 @@ class ExpensesController < ApplicationController
   end
 
   def new
-    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
     @expense = Expense.new(:expense_report => @expense_report)
-    @categories = Category.all
     @expense.comments.build( expense_id: @expense.id )
   end
 
   def create
-    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
     @expense = Expense.new(expense_params)
 
     if @expense.save
       flash[:success] = "Successfully created new expense"
       redirect_to expense_report_expense_path(@expense_report.id, @expense)
     else
-      # @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
-      @categories = Category.all
       render :new
     end
   end
 
   def edit
-    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
-    @expense = Expense.find_by(id: params[:id])
-    @categories = Category.all
-    # binding.pry
   end
 
   def update
-    # binding.pry
-    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
-    @expense = Expense.find_by(id: params[:id])
-
     if @expense.update(expense_params)
       flash[:success] = "Successfully updated expense"
       redirect_to expense_report_expense_path(@expense_report.id, @expense)
     else
       flash[:alert] = "Something went wrong updating this expense. Try again."
-      @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
-      @expense = Expense.find_by(id: params[:id])
       @category = Category.find_by(params[:id])
-      @categories = Category.all
       render :edit
     end
-
   end
 
   def destroy
-    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
-    @expense = Expense.find_by(id: params[:id])
-
     if @expense.destroy
       flash[:success] = "Successfully deleted expense"
       redirect_to expense_report_path(@expense_report)
@@ -73,6 +53,18 @@ class ExpensesController < ApplicationController
 
   def expense_params
     params.require(:expense).permit(:month, :cost, :expense_report_id, :category_id, :comments_attributes => [:description, :expense_id])
+  end
+
+  def set_report
+    @expense_report = ExpenseReport.find_by(id: params[:expense_report_id])
+  end
+
+  def set_expense
+    @expense = Expense.find_by(id: params[:id])
+  end
+
+  def set_categories
+    @categories = Category.all
   end
 
 end
