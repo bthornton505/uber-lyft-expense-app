575ac2824527ff11362d55b2ffc7d299939ecbf4
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-20 14:45:48 -0700

    created next and prev methods to see individual reports

--- app/models/expense_report.rb
+++ app/models/expense_report.rb
@@ -9,8 +9,34 @@ class ExpenseReport < ActiveRecord::Base
   scope :order_by_year, -> { order(year: :desc) }
   scope :by_year, -> (year) { select(:id, :month, :year).where("year = ?", year) }
 
-  # def self.current_expense_report
-  #   self.recently_added.first
-  # end
+  def next(user)
+    reports = user_report_ids_by_year(user)
+    report_index = reports.find_index(id)
+    next_report = report_index + 1
+
+    if next_report < reports.length
+      reports[next_report]
+    else
+      id
+    end
+  end
+
+  def prev(user)
+    reports = user_report_ids_by_year(user)
+    report_index = reports.find_index(id)
+    prev_report = report_index - 1
+
+    if prev_report > 0
+      reports[prev_report]
+    else
+      id
+    end
+  end
+
+  private
+
+  def user_report_ids_by_year(user)
+    @related_report_ids ||= user.expense_reports.order(year: :asc).pluck(:id)
+  end
 
 end
