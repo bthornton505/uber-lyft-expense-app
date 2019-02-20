d7243ef8c5e3dcf70d0f8179ca347588d1ffe3a6
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-19 17:40:46 -0700

    created new expense constructor and added prototype method to create expenses table on a report show page

--- app/assets/javascripts/expense.js
+++ app/assets/javascripts/expense.js
@@ -21,3 +21,24 @@ const addComment = () => {
     e.preventDefault();
   })
 };
+
+function Expense(expense) {
+  this.id = expense.id;
+  this.month = expense.month;
+  this.cost = expense.cost;
+  this.expense_report_id = expense.expense_report_id;
+  this.category = expense.category;
+  this.comments = expense.commments;
+}
+
+Expense.prototype.renderReportExpenses = function() {
+  let reportExpenses = `
+    <tr id="${this.id}">
+      <td class="expense-category">${this['category'].name}</td>
+      <td class="expense-cost">$${this.cost}</td>
+      <td class="expense-link"><a href="/expense_reports/${this.expense_report_id}/expenses/${this.id}">View</a></td>
+    </tr>
+  `
+
+  return reportExpenses;
+}
