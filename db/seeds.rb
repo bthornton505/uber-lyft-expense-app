639bc189d2c83f4498778a13e20efb997a65a34d
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-22 13:24:24 -0700

    created seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,39 +13,54 @@ category_list.each do |category|
   Category.create( name: category )
 end
 
-#This creates the months a user can pick from for expense reports
-# expense_report_months = [
-#   "January",
-#   "February",
-#   "March",
-#   "April",
-#   "May",
-#   "June",
-#   "July",
-#   "August",
-#   "September",
-#   "October",
-#   "November",
-#   "December"
-# ]
-#
-# expense_report_months.each do |month|
-#   ExpenseReport.create( month: month )
-# end
-#
-# expense_report_years = [
-#   2017,
-#   2018,
-#   2019,
-#   2020,
-#   2021,
-#   2022,
-#   2023,
-#   2024,
-#   2025,
-#   2026
-# ]
-#
-# expense_report_years.each do |year|
-#   ExpenseReport.create( year: year )
-# end
+# Creates Users
+User.create(name: 'Brenden', password: 'taxdriver')
+
+# Create Reports
+ExpenseReport.create(month: 'November', year: 2018, user_id: 1)
+ExpenseReport.create(month: 'December', year: 2018, user_id: 1)
+ExpenseReport.create(month: 'January', year: 2019, user_id: 1)
+ExpenseReport.create(month: 'February', year: 2019, user_id: 1)
+ExpenseReport.create(month: 'March', year: 2019, user_id: 1)
+
+# Create Expenses
+Expense.create(month: 'November', cost: 100, category_id: 1, expense_report_id: 1)
+Expense.create(month: 'November', cost: 237, category_id: 3, expense_report_id: 1)
+Expense.create(month: 'November', cost: 130, category_id: 2, expense_report_id: 1)
+
+Expense.create(month: 'December', cost: 100, category_id: 1, expense_report_id: 2)
+Expense.create(month: 'December', cost: 237, category_id: 3, expense_report_id: 2)
+Expense.create(month: 'December', cost: 130, category_id: 2, expense_report_id: 2)
+
+Expense.create(month: 'January', cost: 100, category_id: 1, expense_report_id: 3)
+Expense.create(month: 'January', cost: 237, category_id: 3, expense_report_id: 3)
+Expense.create(month: 'January', cost: 130, category_id: 2, expense_report_id: 3)
+
+Expense.create(month: 'February', cost: 100, category_id: 1, expense_report_id: 4)
+Expense.create(month: 'February', cost: 237, category_id: 3, expense_report_id: 4)
+Expense.create(month: 'February', cost: 130, category_id: 2, expense_report_id: 4)
+
+Expense.create(month: 'March', cost: 100, category_id: 1, expense_report_id: 5)
+Expense.create(month: 'March', cost: 237, category_id: 3, expense_report_id: 5)
+Expense.create(month: 'March', cost: 130, category_id: 2, expense_report_id: 5)
+
+# Create Comments 
+Comment.create(description: 'Gas for the month', expense_id: 1)
+Comment.create(description: 'Paid to Suntrust Bank', expense_id: 2)
+Comment.create(description: 'Geico payment', expense_id: 3)
+
+Comment.create(description: 'Gas for the month', expense_id: 4)
+Comment.create(description: 'Paid to Suntrust Bank', expense_id: 5)
+Comment.create(description: 'Geico payment', expense_id: 6)
+
+Comment.create(description: 'Gas for the month', expense_id: 7)
+Comment.create(description: 'Paid to Suntrust Bank', expense_id: 8)
+Comment.create(description: 'Geico payment', expense_id: 9)
+
+Comment.create(description: 'Gas for the month', expense_id: 10)
+Comment.create(description: 'Paid to Suntrust Bank', expense_id: 11)
+Comment.create(description: 'Geico payment', expense_id: 12)
+
+Comment.create(description: 'Gas for the month', expense_id: 13)
+Comment.create(description: 'Paid to Suntrust Bank', expense_id: 14)
+Comment.create(description: 'Geico payment', expense_id: 15)
