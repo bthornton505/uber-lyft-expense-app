29e635e1c364f643cf0fa65f5e3f63d3bc2d9263
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-22 13:24:11 -0700

    removed categories instance varialbe in show action

--- app/controllers/expense_reports_controller.rb
+++ app/controllers/expense_reports_controller.rb
@@ -19,9 +19,9 @@ class ExpenseReportsController < ApplicationController
 
   def show
     @expenses = Expense.all.select {|e| e.expense_report_id == @expense_report.id}
-    @categories = Expense.includes(:category).map {|e| e.category if e.expense_report_id == @expense_report.id }
     @next_report = @expense_report.next(@user)
     @prev_report = @expense_report.prev(@user)
+    
     respond_to do |format|
       format.html { render :show }
       format.json { render json: @expense_report, status: 201 }
