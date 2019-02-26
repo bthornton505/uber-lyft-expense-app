5644e4438d98975ab48f5d750e096f5b677695a8
Brenden Thornton <thorntonbrenden@gmail.com>
2019-02-26 11:26:24 -0700

    updated expense object to use class constructor syntax

--- app/assets/javascripts/expense.js
+++ app/assets/javascripts/expense.js
@@ -22,23 +22,48 @@ const addComment = () => {
   })
 };
 
-function Expense(expense) {
-  this.id = expense.id;
-  this.month = expense.month;
-  this.cost = expense.cost;
-  this.expense_report_id = expense.expense_report_id;
-  this.category = expense.category;
-  this.comments = expense.commments;
+class Expense {
+  constructor(id, month, cost, expense_report_id, category, comments) {
+    this.id = id;
+    this.month = month;
+    this.cost = cost;
+    this.expense_report_id = expense_report_id;
+    this.category = category;
+    this.comments = commments;
+  }
+
+  renderReportExpenses() {
+    let reportExpenses = `
+      <tr id="${this.id}">
+        <td class="expense-category">${this['category'].name}</td>
+        <td class="expense-cost">$${this.cost}</td>
+        <td class="expense-link"><a href="/expense_reports/${this.expense_report_id}/expenses/${this.id}">View</a></td>
+      </tr>
+    `
+
+    return reportExpenses;
+  }
 }
 
-Expense.prototype.renderReportExpenses = function() {
-  let reportExpenses = `
-    <tr id="${this.id}">
-      <td class="expense-category">${this['category'].name}</td>
-      <td class="expense-cost">$${this.cost}</td>
-      <td class="expense-link"><a href="/expense_reports/${this.expense_report_id}/expenses/${this.id}">View</a></td>
-    </tr>
-  `
+// OLD VERSION OF JS OBJECT
 
-  return reportExpenses;
-}
+// function Expense(expense) {
+//   this.id = expense.id;
+//   this.month = expense.month;
+//   this.cost = expense.cost;
+//   this.expense_report_id = expense.expense_report_id;
+//   this.category = expense.category;
+//   this.comments = expense.commments;
+// }
+//
+// Expense.prototype.renderReportExpenses = function() {
+//   let reportExpenses = `
+//     <tr id="${this.id}">
+//       <td class="expense-category">${this['category'].name}</td>
+//       <td class="expense-cost">$${this.cost}</td>
+//       <td class="expense-link"><a href="/expense_reports/${this.expense_report_id}/expenses/${this.id}">View</a></td>
+//     </tr>
+//   `
+//
+//   return reportExpenses;
+// }
